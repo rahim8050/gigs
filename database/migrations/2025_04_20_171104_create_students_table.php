89caad494d18ac5467a928a9712a7b8205282965
rahim8050 <rahimranxx8050@gmail.com>
2025-04-20 18:11:53 +0100

    deletions & insertions

--- database/migrations/2025_04_20_171104_create_students_table.php
+++ database/migrations/2025_04_20_171104_create_students_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_students', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('studentname');
             $table->string('studentemail')->unique();
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('table_students');
+        Schema::dropIfExists('students');
     }
 };
